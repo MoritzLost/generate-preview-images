60a0a2a843470c1ebb2271a9d3c2cc6c6b76dfc6
Moritz L'Hoest <info@herebedragons.world>
2020-10-05 21:35:30 +0200

    Fix several bugs and add more options

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const fg = require('fast-glob');
 const puppeteer = require('puppeteer');
 const express = require('express');
@@ -6,11 +7,24 @@ const express = require('express');
 const generatePreviewImages = async (dir, options = {}) => {
     const finalOptions = Object.assign(
         {
-            globPattern: '**/*.{html.html}'
+            globPattern: '**/*.{html,html}',
+            serverPort: 3000,
+            viewport: { width: 480, height: 360 },
+            screenshotOptions: {
+                type: 'png',
+                fullPage: false,
+            },
+            screenshotElementSelector: false,
+            pageWaitUntil: 'networkidle0',
+            // ouputPath: (filePath, ext) => path.resolve(dir, `${filePath.replace('/', '___')}.${ext}`),
+            outputPath: (baseDir, filePath, ext) => path.resolve(
+                baseDir,
+                `${filePath.replace(/\.[a-zA-Z]+$/, `.${ext}`)}`
+            ),
+            removeOriginalFiles: false,
         },
         options
     );
-
     const pages = await fg(finalOptions.globPattern, {
         cwd: dir
     });
@@ -18,20 +32,43 @@ const generatePreviewImages = async (dir, options = {}) => {
         headless: true,
     });
 
-    // https://github.com/puppeteer/puppeteer/issues/1643#issuecomment-353387148
+    // start a server to serve the both the HTML files as well as any static files such as CSS
     const app = express();
     app.use(express.static(dir));
-    const server = app.listen(3000);
+    const server = app.listen(finalOptions.serverPort);
 
     for (const file of pages) {
-        console.log(file);
+        // create a browser page and set it's viewport to the specified dimensions
         const page = await browser.newPage();
-        await page.setViewport({ width: 480, height: 360 });
-        await page.goto(`http://localhost:3000/${file}`);
-        const buffer = await page.screenshot({path: `images/${file.replace('/', '___')}.png`});
-        console.log(buffer);
+        await page.setViewport(finalOptions.viewport);
+        // navigate to the current page
+        await page.goto(
+            `http://localhost:${finalOptions.serverPort}/${file}`,
+            { waitUntil: finalOptions.pageWaitUntil }
+        );
+        const screenshotBase = finalOptions.screenshotElementSelector
+            ? await page.$(finalOptions.screenshotElementSelector)
+            : page;
+        // use callback to generate the output path
+        const outputPath = finalOptions.outputPath(
+            dir,
+            file,
+            finalOptions.screenshotOptions.type || 'png'
+        );
+        // create the directory recursively (if it doesn't exist already)
+        fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+        // take a screenshot
+        const buffer = await screenshotBase.screenshot({
+            path: outputPath,
+            ...finalOptions.screenshotOptions
+        });
+        if (finalOptions.removeOriginalFiles) {
+            fs.unlinkSync(path.resolve(dir, file));
+        }
     }
     server.close();
+    await browser.close();
+    // @TODO: return list of files or whatever?
 }
 
 exports.default = generatePreviewImages;
